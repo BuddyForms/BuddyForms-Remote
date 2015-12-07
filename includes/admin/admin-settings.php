112056fcea1a03c44b7f9afab1b48473bd5adfdf
Sven Lehnert <mail@sven-lehnert.de>
2015-12-07 13:32:25 +0100

    Create the Embed Generator First working prototype

--- includes/admin/admin-settings.php
+++ includes/admin/admin-settings.php
@@ -23,11 +23,46 @@ function buddyforms_remote_create_sub_menu(){
 add_action('admin_menu', 'buddyforms_remote_create_sub_menu');
 
 function bf_remote_screen(){
+    global $buddyforms;
 
     // Check that the user is allowed to update options
     if (!current_user_can('manage_options')) {
         wp_die(__('You do not have sufficient permissions to access this page.','buddyforms'));
     } ?>
+
+    <script>
+
+        jQuery(document).ready(function (){
+            jQuery(document).on( "click", '#buddyforms_toggle_generate', function( event ) {
+
+                var form_slug = jQuery('#buddyforms_toggle_form').val();
+                if(form_slug == 'none'){
+                    alert('Please Select a form');
+                    return false;
+                }
+
+                var bf_action = jQuery('#buddyforms_toggle_action').val();
+                if(bf_action == 'none'){
+                    alert('Please Select a Action');
+                    return false;
+                }
+
+                jQuery.ajax({
+                    type: 'POST',
+                    url: ajaxurl,
+                    data: {"action": "buddyforms_remote_toggle_generate", "form_slug": form_slug, "bf_action": bf_action },
+                    success: function(data){
+                        if(data != 0)
+                            jQuery("textarea#buddyforms_toggle_result").text(data);
+
+                    },
+                    error: function (request, status, error) {
+                        alert(request.responseText);
+                    }
+                });
+            });
+        });
+    </script>
 
     <div id="bf_admin_wrap" class="wrap">
         <?php include(BUDDYFORMS_INCLUDES_PATH . 'admin/admin-credits.php'); ?>
@@ -47,11 +82,50 @@ function bf_remote_screen(){
         </p>
 
         <h3>Create Toggle Embed Code</h3>
-        select form/select action
+        <p>Select the Form and Action</p>
+        <br>
+        <?php
 
-        http://buddyforms/sample-page/bf-api/sadsa/?action=create
 
+        ?>
+        <input type="hidden" value="<?php echo get_bloginfo('url') ?>">
+        <select id="buddyforms_toggle_form">
+            <option value="none">Select Form</option>
+            <?php foreach ($buddyforms as $form_key => $form) { ?>
+                <option value="<?php echo $form_key ?>"><?php echo $form['name'] ?></option>
+            <?php } ?>
+        </select>
+        <select id="buddyforms_toggle_action">
+            <option value="none">Select Action</option>
+            <option value="create">Create</option>
+            <option value="view">View Post List</option>
+        </select>
+        <a id="buddyforms_toggle_generate" href="#" >Generate Embed Code</a>
+        <br><br>
+        <textarea rows="3" cols="100" id="buddyforms_toggle_result"></textarea>
+        <p><i class="icon-check"></i> After Generate copy and paste the above code before the <code>&lt;/body&gt;</code> tag on your website.</p>
     </div>
 
     <?php
 }
+
+function buddyforms_remote_toggle_generate(){
+    global $buddyforms;
+
+    if(!isset($_POST['form_slug']) || $_POST['form_slug'] == 'none')
+        return;
+    if(!isset($_POST['bf_action']) || $_POST['bf_action'] == 'none')
+        return;
+
+    $form_slug      = $_POST['form_slug'];
+    $bf_action      = $_POST['bf_action'];
+
+    $attached_page  = get_post($buddyforms[$form_slug]['attached_page']);
+    $attached_page  = $attached_page->post_name;
+
+    $url = get_bloginfo('url') . '/' . $attached_page . '/bf-api/' . $form_slug . '?action=' . $bf_action;
+
+    echo "<script type='text/javascript' src='" . $url . "'></script>";
+    die();
+}
+add_action('wp_ajax_buddyforms_remote_toggle_generate', 'buddyforms_remote_toggle_generate');
