63629eefff5b0e7ccd48e0053d179abe97460ecd
Sven Lehnert <mail@sven-lehnert.de>
2016-02-26 11:59:16 +0100

    create new form builder meta box to enable remote and generate the embed code
    remove unneeded code

--- includes/admin/admin-settings.php
+++ includes/admin/admin-settings.php
@@ -1,103 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: svenl77
- * Date: 25.03.14
- * Time: 14:44
- */
-
-/**
- * Create "BuddyForms Options" nav menu
- *
- * @package buddyforms
- * @since 0.1-beta
- */
-
-function bf_remote_screen(){
-    global $buddyforms;
-
-    // Check that the user is allowed to update options
-    if (!current_user_can('manage_options')) {
-        wp_die(__('You do not have sufficient permissions to access this page.','buddyforms'));
-    } ?>
-
-    <script>
-
-        jQuery(document).ready(function (){
-            jQuery(document).on( "click", '#buddyforms_toggle_generate', function( event ) {
-
-                var form_slug = jQuery('#buddyforms_toggle_form').val();
-                if(form_slug == 'none'){
-                    alert('Please Select a form');
-                    return false;
-                }
-
-                var bf_action = jQuery('#buddyforms_toggle_action').val();
-                if(bf_action == 'none'){
-                    alert('Please Select a Action');
-                    return false;
-                }
-
-                jQuery.ajax({
-                    type: 'POST',
-                    url: ajaxurl,
-                    data: {"action": "buddyforms_remote_toggle_generate", "form_slug": form_slug, "bf_action": bf_action },
-                    success: function(data){
-                        if(data != 0)
-                            jQuery("textarea#buddyforms_toggle_result").text(data);
-
-                    },
-                    error: function (request, status, error) {
-                        alert(request.responseText);
-                    }
-                });
-            });
-        });
-    </script>
-
-    <div id="bf_admin_wrap" class="wrap">
-        <?php include(BUDDYFORMS_INCLUDES_PATH . 'admin/admin-credits.php'); ?>
-        <h2>BuddyForms Remote</h2>
-        <h3>Different integrations</h3>
-
-        <p><b>URL End Point</b></p>
-        <p>You can use URL EndPoints to display your form without your theme. This is perfect to create a FaceBook App or us in iFrames.</p>
-
-        <p><b>Toggle Embed Code</b></p>
-        <p>
-          </p>
-
-        <h3>Create Toggle Embed Code</h3>
-        <p>Select the Form and Action</p>
-        <br>
-        <?php
-
-
-        ?>
-        <input type="hidden" value="<?php echo get_bloginfo('url') ?>">
-        <select id="buddyforms_toggle_form">
-            <option value="none">Select Form</option>
-            <?php foreach ($buddyforms as $form_key => $form) {
-
-                if(!isset($form['remote']))
-                    continue;
-                ?>
-                <option value="<?php echo $form_key ?>"><?php echo $form['name'] ?></option>
-            <?php } ?>
-        </select>
-        <select id="buddyforms_toggle_action">
-            <option value="none">Select Action</option>
-            <option value="create">Create</option>
-            <option value="view">View Post List</option>
-        </select>
-        <a id="buddyforms_toggle_generate" href="#" >Generate Embed Code</a>
-        <br><br>
-        <textarea rows="3" cols="100" id="buddyforms_toggle_result"></textarea>
-        <p><i class="icon-check"></i> After Generate copy and paste the above code before the <code>&lt;/body&gt;</code> tag on your website.</p>
-    </div>
-
-    <?php
-}
 
 function buddyforms_remote_toggle_generate(){
     global $buddyforms;
@@ -118,7 +19,6 @@ function buddyforms_remote_toggle_generate(){
     echo "<script type='text/javascript' src='" . $url . "'></script>";
     die();
 }
-add_action('wp_ajax_buddyforms_remote_toggle_generate', 'buddyforms_remote_toggle_generate');
 
 
 function buddyforms_remote_add_button_to_submit_box() {
--- includes/admin/form-elements.php
+++ includes/admin/form-elements.php
@@ -1,6 +1,6 @@
 <?php
 function buddyforms_remote_admin_settings_sidebar_metabox(){
-    add_meta_box('buddyforms_remote', __("Remote",'buddyforms'), 'buddyforms_remote_admin_settings_sidebar_metabox_html', 'buddyforms', 'side', 'low');
+    add_meta_box('buddyforms_remote', __("Remote",'buddyforms'), 'buddyforms_remote_admin_settings_sidebar_metabox_html', 'buddyforms', 'advanced', 'low');
 }
 
 function buddyforms_remote_admin_settings_sidebar_metabox_html(){
@@ -11,84 +11,42 @@ function buddyforms_remote_admin_settings_sidebar_metabox_html(){
 
     $buddyform = get_post_meta(get_the_ID(), '_buddyforms_options', true);
 
-
     $form_setup = array();
 
     $remote = 'off';
     if(isset($buddyform['remote']))
         $remote = $buddyform['remote'];
 
+    $form_setup[] = new Element_Checkbox("<p>" . __('Enabled Remote to generate new endpoinds and the toggle for this form. You need to save the form onece to generate the new endpoints and toggle embed codes.', 'buddyforms') . "</p>", "buddyforms_options[remote]", array("remote" => "Enable Remote"), array('value' => $remote));
 
-    $form_setup[] = new Element_Checkbox("<b>" . __('Enable Remote', 'buddyforms') . "</b>", "buddyforms_options[remote]", array("remote" => "Remote"), array('value' => $remote, 'shortDesc' => __('If Remote is enabled all needed ent pints will be generated and the Form can be used to create a Toggle', 'buddyforms')));
+    $form_setup[] = new Element_HTML(__('', 'buddyforms'));
 
+    if($remote != 'off'){
 
-    ?>
+      $form_setup[] = new Element_HTML('
+            <br><p><b>Toggle Embed Codes</b></p>
+          ');
 
-    <script>
+      $attached_page  = get_post($buddyform['attached_page']);
+      $attached_page  = $attached_page->post_name;
 
-        jQuery(document).ready(function (){
-            jQuery(document).on( "click", '#buddyforms_toggle_generate', function( event ) {
+      $form_slug = $buddyform['slug'];
 
-                var form_slug = jQuery('#buddyforms_toggle_form').val();
-                if(form_slug == 'none'){
-                    alert('Please Select a form');
-                    return false;
-                }
+      $url = get_bloginfo('url') . '/' . $attached_page . '/bf-api/' . $form_slug . '?action=create';
+      $url = "<script type='text/javascript' src='" . $url . "'></script>";
+      $form_setup[] = new Element_HTML(' Create embed code: <input type="text" onFocus="this.focus();this.select()" value="' . $url . '" />');
 
-                var bf_action = jQuery('#buddyforms_toggle_action').val();
-                if(bf_action == 'none'){
-                    alert('Please Select a Action');
-                    return false;
-                }
+      $url = get_bloginfo('url') . '/' . $attached_page . '/bf-api/' . $form_slug . '?action=view';
+      $url = "<script type='text/javascript' src='" . $url . "'></script>";
+      $form_setup[] = new Element_HTML(' View embed code: <input type="text" onFocus="this.focus();this.select()" value="' . $url . '" />');
 
-                jQuery.ajax({
-                    type: 'POST',
-                    url: ajaxurl,
-                    data: {"action": "buddyforms_remote_toggle_generate", "form_slug": form_slug, "bf_action": bf_action },
-                    success: function(data){
-                        if(data != 0)
-                            jQuery("textarea#buddyforms_toggle_result").text(data);
+      $form_setup[] = new Element_HTML('
+          <br><p><b>URL Endponds</b></p>
+          <p>You can use URL endpoints to display your forms in iframes. Two now Buttons have been added to the Publish Sitebar MetaBox</p>
+          ');
 
-                    },
-                    error: function (request, status, error) {
-                        alert(request.responseText);
-                    }
-                });
-            });
-        });
-    </script>
-    <?php
-    $form_setup[] = new Element_HTML('
-        <h3>Different integrations</h3>
-        <p><b>URL End Point</b></p>
-        <p>You can use URL EndPoints to display your form without your theme. This is perfect to create a FaceBook App or us in iFrames.</p>
-        <p><b>Toggle Embed Code</b></p>
-        <h3>Create Toggle Embed Code</h3>
-        <p>Select the Form and Action</p>
-        <br>
-    <input type="hidden" value="<?php echo get_bloginfo(\'url\') ?>">
-    <select id="buddyforms_toggle_form">
-        <option value="none">Select Form</option>
-    ');
+    }
 
-     foreach ($buddyforms as $form_key => $form) {
-
-        if(!isset($form['remote']))
-            continue;
-
-         $form_setup[] = new Element_HTML('   <option value="<?php echo $form_key ?>">' . $form['name'] . '</option>');
-     }
-
-    $form_setup[] = new Element_HTML('</select>
-    <select id="buddyforms_toggle_action">
-        <option value="none">Select Action</option>
-        <option value="create">Create</option>
-        <option value="view">View Post List</option>
-    </select>
-    <a id="buddyforms_toggle_generate" href="#" >Generate Embed Code</a>
-    <br><br>
-    <textarea rows="3" cols="100" id="buddyforms_toggle_result"></textarea>
-    <p><i class="icon-check"></i> After Generate copy and paste the above code before the <code>&lt;/body&gt;</code> tag on your website.</p>');
 
     foreach($form_setup as $key => $field){
         echo '<div class="buddyforms_field_label">' . $field->getLabel() . '</div>';
