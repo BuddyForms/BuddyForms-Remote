637140c75dc7fab3b15fece54ab959920f82f594
Sven Lehnert <mail@sven-lehnert.de>
2016-09-18 15:20:45 +0200

    create new functions to show hide metaboxes work on the conditionals admin ui

--- includes/admin/form-elements.php
+++ includes/admin/form-elements.php
@@ -2,6 +2,8 @@
 function buddyforms_remote_admin_settings_sidebar_metabox(){
     add_meta_box('buddyforms_remote', __("Remote",'buddyforms'), 'buddyforms_remote_admin_settings_sidebar_metabox_html', 'buddyforms', 'advanced', 'low');
     add_filter('postbox_classes_buddyforms_buddyforms_remote','buddyforms_metabox_class');
+    add_filter('postbox_classes_buddyforms_buddyforms_remote','buddyforms_metabox_show_if_form_type_posts');
+    add_filter('postbox_classes_buddyforms_buddyforms_remote','buddyforms_metabox_show_if_post_type_none');
 }
 
 function buddyforms_remote_admin_settings_sidebar_metabox_html(){
